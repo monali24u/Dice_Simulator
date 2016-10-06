e52ef8aca747c1cd71ac14a3ce836f2bddba2768
monali24u <monali24u@gmail.com>
2016-10-06 16:44:24 -0600

    Updated comments
    Added algorithm for how the throw outcomes are calculated when one side
    of a dire is laoded with certain amount

--- DiceEngine.cpp
+++ DiceEngine.cpp
@@ -1,3 +1,4 @@
+
 #include <sstream>
 #include <iostream>
 #include "DiceSimulator.h"
@@ -17,7 +18,24 @@ void DiceEngine::CalculateDiceResult(DiceSimulator *ptr_DS)
 	//generate random dice result
 	CalculateThrows(ptr_DS, result_vec, percent_vec);
 }
-//
+
+//Input - Dice data and number of throws
+//Output - dice outcome and percentages
+
+//Data is passed by referance. 
+//For each dice check the loadedside=loadedAmount pair, and find default size (Basecase Die1, loadedside=3, loadedAmount= 2)
+//Make up imaginary array consisting of sides of the dice other than loadedside. Now Add the loadedside, loadedAmount number of times.
+//All the sides will be considered in order.
+//Resultant imaginary array will be [1, 2, 3, 3, 4, 5, 6] with index from 0 to 6(For Basecase input - default size will be((6 + 2 - 1) = 7)).
+// std::rand() function generates random values between two numbers.
+// For this example the range to randwill be bewtween 1 and default size. 
+//For each throw using std::rand() function and default size random array index is generated.
+//value at the index is the result of the throw.
+//Note - An actual array need not be created. Since the frequency of loadedside is known,
+//the actual values at the array can be calculated without using a actual array
+//Since additional loadedside valve is added loadedAmount times, the probability occurance of the loaded side is more likely.
+
+
 void DiceEngine::CalculateThrows(DiceSimulator *ptr_DS, vector<ThrowDisplay> &result_vec, vector<PercentageDisplay> &percent_vec)
 {
 	vector<Dice> newDiceVec = ptr_DS->GetDiceContainer();
@@ -36,7 +54,6 @@ void DiceEngine::CalculateThrows(DiceSimulator *ptr_DS, vector<ThrowDisplay> &re
 		//For calculation of percentage
 		percent_vec[j].diceName = obj.GetDiceName();
 
-		//number of rolls = 10
 		for(int i = 0; i < ptr_DS->GetNumberOfThrows(); i++)
 		{
 			//For calculation of output
